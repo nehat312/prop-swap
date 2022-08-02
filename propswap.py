9a6d5149a85e16571f5b6f0b0a4f1afab4c7d3d8
nehat312 <nate.ehat@gmail.com>
2022-08-02 01:36:20 -0400

    Update propswap.py

--- propswap.py
+++ propswap.py
@@ -402,7 +402,16 @@ with st.form("PROPERTY PARAMETERS"):
             st.write(f'{per_unit_valuation:.0f}')
 
 
+#######################
+## TABLEAU EMBEDDING ##
+#######################
 
+def main():
+    html_temp = """<div class='tableauPlaceholder' id='viz1659418206902' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RE&#47;RE_INVESTORS&#47;INVESTORS&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='RE_INVESTORS&#47;INVESTORS' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RE&#47;RE_INVESTORS&#47;INVESTORS&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1659418206902');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
+    components.html(html_temp)
+
+if __name__ == "__main__":
+    main()
 
 
 ## EXTERNAL LINKS
@@ -414,18 +423,6 @@ if left_button:
     left_column.write('https://public.tableau.com/shared/S4GKR7QYB?:display_count=n&:origin=viz_share_link')
 if right_button:
     right_column.write('https://github.com/nehat312/prop-swap')
-
-
-#######################
-## TABLEAU EMBEDDING ##
-#######################
-
-def main():
-    html_temp = """<div class='tableauPlaceholder' id='viz1659418206902' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RE&#47;RE_INVESTORS&#47;INVESTORS&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='RE_INVESTORS&#47;INVESTORS' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RE&#47;RE_INVESTORS&#47;INVESTORS&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1659418206902');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*1.25)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
-    components.html(html_temp)
-
-if __name__ == "__main__":
-    main()
 
 
 # st.success('THANKS FOR PROP/SWAPPING')
